1a1f4753aa21bd90e85871c4771ce676769b7398
Takumi Yamashita <64026753+husty530@users.noreply.github.com>
2022-07-21 16:45:40 +0900

    *

--- WpfMaterialApp/MainWindow.xaml.cs
+++ WpfMaterialApp/MainWindow.xaml.cs
@@ -1,24 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.WindowsAPICodePack.Dialogs;
-using Path = System.IO.Path;
 using OpenCvSharp;
 using OpenCvSharp.WpfExtensions;
-using Husty.OpenCvSharp;
-using System.Threading;
+using Path = System.IO.Path;
 
 namespace WpfMaterialApp
 {
